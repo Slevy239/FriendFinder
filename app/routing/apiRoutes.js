35d176561573a8ca41794b29bb0d555ad31f8c0b
Sam Levy <slevy239@gmail.com>
2019-11-04 16:55:50 -0500

    api

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,38 +1,12 @@
-var friends = require("../data/friends")
+var friends = require("../data/friends");
 
-module.exports = function(app) {
-    app.get("api/friends", function(req, res) {
+
+module.exports = function (app) {
+    app.get("/api/friends", function (req, res) {
         res.json(friends);
     });
 
     app.post("/api/friends", function(req, res) {
-        console.log(req.body.scores);
-        var user = req.body;
-
-        for(var i = 0; i < user.scores.length; i++) {
-            user.scores[i] = parseInt(user.scores[i]);
-        }
-
-        var bestFriendIndex = 0;
-        var minDiff = 40;
-        
-        for (var i = 0; i < friends.length; i++) {
-            var totalDiff = 0;
-            for (var j = 0; j < friends[i].scores.length; j++) {
-                var difference = Math.abs(user.scores[j] - friends[i].scores[j]);
-                totalDiff += difference;
-            }
-
-
-            if (totalDiff < minDiff) {
-                bestFriendIndex = i;
-                minDiff = totalDiff;
-            }
-        }
-        friends.push(user);
-
-        res.json(friends[bestFriendIndex]);
-
+        friends.push(req.body);
     })
 }
-
