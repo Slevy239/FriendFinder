549aee3aae4f2e33d6013337908e3115000bdfe3
Sam Levy <slevy239@gmail.com>
2019-11-06 12:43:52 -0500

    console

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,11 +4,13 @@ var friends = require("../data/friends");
 module.exports = function (app) {
     app.get("/api/friends", function (req, res) {
         res.json(friends);
+        console.log("get")
     });
 
 
 
     app.post("/api/friends", function (req, res) {
+        console.log("post")
         var bodyData = req.body;
         var currentScore = bodyData.scores;
 
