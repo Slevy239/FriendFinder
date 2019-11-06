3f86a4c3bff23f416350f7fb01fc5ab1cb7900f7
Sam Levy <slevy239@gmail.com>
2019-11-06 13:02:35 -0500

    redo

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,53 +4,36 @@ var friends = require("../data/friends");
 module.exports = function (app) {
     app.get("/api/friends", function (req, res) {
         res.json(friends);
-        console.log("get")
     });
 
 
 
     app.post("/api/friends", function (req, res) {
-        console.log("post")
-        var bodyData = req.body;
-        var currentScore = bodyData.scores;
+        console.log(req.body.scores);
+        var user = req.body;
 
+        for (var i = 0; i < user.scores.length; i++) {
+            user.scores[i] = parseInt(user.scores[i]);
 
-        var match = {
-            name: "",
-            img: ""
         }
 
-        var diff = 0;
+        var index = 0;
+        var minDiff = 40;
 
-        if (currentScore === undefined || currentScore.length === 0) {
-            console.log('ending the program due to bad POST request' +
-                '\n' + 'user submitted survey without selecting answer to a question');
-
-            res.end();
-        } else {
-            friends.push(bodyData);
-
-            for (var i = 0; i < friends.length - 1; i++) {
-                for (var i = 0; i <currentScore.length; j++) {
-                    diff += Math.abs(currentScore[j] - friends[i].scores[j])
-                }
-                friends[i]["matchScore"] = diff;
-                diff = 0;
+        for (var i = 0; i < friends.length; i++) {
+            var totalDifference = 0;
+            for (var j = 0; j < friends[i].scores.length; j++) {
+                var difference = Math.abs(user.scores[j] - friends[i].scores[j]);
+                totalDifference += difference;
             }
-            var lowScore = [];
-            for (var i = 0; i <friends.length - 1; i++) {
-                lowScore.push(parseInt(friendsp[i].matchScore));
+            if (totalDifference < minDiff) {
+                index = i;
+                minDiff = totalDifference;
             }
-            var lowNum = Math.min(...lowScore);
-            var lowNumIndex = lowScore.indexOf(lowNum)
-
-            match.name = friends[lowNumIndex].name;
-            match.img = friends[lowNumIndex].img;
-            console.log(match);
-
-
-            res.json(match);
         }
-        // friends.push(req.body);
+        friends.push(user)
+        res.json(friends[index]);
+
+
     })
 }
