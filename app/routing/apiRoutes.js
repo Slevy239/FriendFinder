806c2bdad2da644adf5b69f963c0d54597c1281a
Sam Levy <slevy239@gmail.com>
2019-11-06 12:25:55 -0500

    match

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,7 +6,49 @@ module.exports = function (app) {
         res.json(friends);
     });
 
-    app.post("/api/friends", function(req, res) {
-        friends.push(req.body);
+
+
+    app.post("/api/friends", function (req, res) {
+        var bodyData = req.body;
+        var currentScore = bodyData.scores;
+
+
+        var match = {
+            name: "",
+            img: ""
+        }
+
+        var diff = 0;
+
+        if (currentScore === undefined || currentScore.length === 0) {
+            console.log('ending the program due to bad POST request' +
+                '\n' + 'user submitted survey without selecting answer to a question');
+
+            res.end();
+        } else {
+            friends.push(bodyData);
+
+            for (var i = 0; i < friends.length - 1; i++) {
+                for (var i = 0; i <currentScore.length; j++) {
+                    diff += Math.abs(currentScore[j] - friends[i].scores[j])
+                }
+                friends[i]["matchScore"] = diff;
+                diff = 0;
+            }
+            var lowScore = [];
+            for (var i = 0; i <friends.length - 1; i++) {
+                lowScore.push(parseInt(friendsp[i].matchScore));
+            }
+            var lowNum = Math.min(...lowScore);
+            var lowNumIndex = lowScore.indexOf(lowNum)
+
+            match.name = friends[lowNumIndex].name;
+            match.img = friends[lowNumIndex].img;
+            console.log(match);
+
+
+            res.json(match);
+        }
+        // friends.push(req.body);
     })
 }
--- server.js
+++ server.js
@@ -10,9 +10,20 @@ app.use(express.urlencoded({ extended: true}));
 require("./app/routing/apiRoutes.js")(app);
 require("./app/routing/htmlRoutes.js")(app);
 
+app.get("/api/Friendslist", function(req, res) {
+    return res.json(surveyResponse);
+})
+
+
+
+app.post("/api/firendslist", function(req, res) {
+    surveyResponse.push(req.body);
+    console.log(surveyResponse);
+    res.end();
+});
 
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
     console.log("http://localhost:" + PORT);
-})
+});
