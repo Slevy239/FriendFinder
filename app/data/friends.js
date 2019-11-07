75639077f8b14abfe0ea5bed69f2a666d0cd6785
Sam Levy <slevy239@gmail.com>
2019-11-06 19:15:54 -0500

    photo

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,27 +1,27 @@
 var friendsArr = [
     {
         name: "Eric Cartman",
-        photo: "https://vignette.wikia.nocookie.net/southpark/images/c/c4/Eric-cartman.png/revision/latest?cb=20180718082504",
+        img: "https://vignette.wikia.nocookie.net/southpark/images/c/c4/Eric-cartman.png/revision/latest?cb=20180718082504",
         scores: [2,5,7,8,2,3,8,4,5,3]
     },
     {
         name: "Kyle Broflovski",
-        photo: "https://vignette.wikia.nocookie.net/southpark/images/9/95/Kyle-broflovski.png/revision/latest?cb=20190411033301",
+        img: "https://vignette.wikia.nocookie.net/southpark/images/9/95/Kyle-broflovski.png/revision/latest?cb=20190411033301",
         scores: [2,1,4,1,5,5,6,7,3,4]
     },
     {
         name: "Stan Marsh",
-        photo: "http://southparkstudios.mtvnimages.com/shared/characters/kids/stan-marsh.png",
+        img: "http://southparkstudios.mtvnimages.com/shared/characters/kids/stan-marsh.png",
         scores: [2,4,1,1,5,5,8,7,9,2]
     },
     {
         name: "Kenny McCormick",
-        photo: "https://upload.wikimedia.org/wikipedia/en/6/6f/KennyMcCormick.png",
+        img: "https://upload.wikimedia.org/wikipedia/en/6/6f/KennyMcCormick.png",
         scores: [4,4,2,5,6,8,4,3,3,5]
     },
     {
         name: "Chef",
-        photo: "https://upload.wikimedia.org/wikipedia/en/1/1e/SouthParkChef.png",
+        img: "https://upload.wikimedia.org/wikipedia/en/1/1e/SouthParkChef.png",
         scores: [4,4,2,5,7,8,4,2,4,1]
 
     }
